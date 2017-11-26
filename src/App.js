774fb872d89a18e404815cc5ac11ddb0629b2a4d
gingleton <georgeingleton@gmail.com>
2017-11-26 08:47:17 -0500

    Lab: Intro to Props

--- src/App.js
+++ src/App.js
@@ -1,71 +1,42 @@
-// Notice that below, I did not include , {Component} after import
-// React
 import React from 'react';
 
-// we're importing the component from Todo.js
 import Todo from './Todo';
-
 
 class App extends React.Component {
 
-// Establish a function that we are going to be passing
-// into the Todo Component. 
-  fun(e){
-    alert("We're about to get White Castle");
+  fun(event){
+    alert("Let's stay away from White Castle");
   }
 
-  render() {
+  handleClick(event) {
+    console.log(event.target.value)
+  }
 
+
+  render(){
+    //debugger
     return (
       <div>
-        <Todo name="Pick up My cousins kids"
-              age="24" data={data}
-              alert={this.fun} />
-      </div>
-    );
+      <Todo alert ={this.fun}/>
+
+      <input onChange={(event) => this.handleClick(event)} />
+
+      <button onClick={() => this.fun()}>Ring the alarm</button>
+    </div>
+    )
   }
 }
 
-// inputting an object.
-const data = {'todo': [1,2,3,4]}
 
-// passing an array
-// const data = [1,2,3,4]
 
-// passing a number
-// const data = 4
 
-// inserting objects within an array
-// App.defaultProps=[{
-//   name: "whatever",
-//   age: 24,
-//   single: true
-//
-// }]
-
-// Just an object with properties
-// App.defaultProps={
-//   name: "whatever",
-//   age: 24,
-//   single: true
-//
-// }
-
-// Passing an object with one key and two elements in an array
 App.defaultProps={
-
-  "status": [{
-  name: "whatever",
-  age: 24,
-  single: true
-  },
-  {
-    name: "whomever",
-    age: 99,
-    single: false
+  allTasks: [{
+  task: 'do your homework',
+  importance: '1',
+  completed: false
   }]
-
-
 }
+
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -5,12 +5,14 @@ import React from 'react';
 class Todo extends React.Component{
 
 render(){
+  debugger
   return(
     <div>
+      <button onClick={(event) => this.props.alert(event)}>
+        Alert Me
+      </button>
     </div>
   )
-
-
 }
 
 }
